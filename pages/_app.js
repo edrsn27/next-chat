af4a19c975130fe354759b0181e9117f59d2387b
ederson <edersonvillegas50@gmail.com>
2022-03-24 19:22:31 +0800

    initial config for REALM

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,62 @@
+import * as Realm from "realm-web";
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+
 // import App from 'next/app'
 
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+// Connect to your MongoDB Realm app
+const app = new Realm.App(process.env.REALM_APP_ID);
+
+// Gets a valid Realm user access token to authenticate requests
+async function getValidAccessToken() {
+  // Guarantee that there's a logged in user with a valid access token
+  if (!app.currentUser) {
+    // If no user is logged in, log in an anonymous user. The logged in user will have a valid
+    // access token.
+    await app.logIn(Realm.Credentials.anonymous());
+  } else {
+    // An already logged in user's access token might be stale. To guarantee that the token is
+    // valid, we refresh the user's custom data which also refreshes their access token.
+    await app.currentUser.refreshCustomData();
   }
-  
-  // Only uncomment this method if you have blocking data requirements for
-  // every single page in your application. This disables the ability to
-  // perform automatic static optimization, causing every page in your app to
-  // be server-side rendered.
-  //
-  // MyApp.getInitialProps = async (appContext) => {
-  //   // calls page's `getInitialProps` and fills `appProps.pageProps`
-  //   const appProps = await App.getInitialProps(appContext);
-  //
-  //   return { ...appProps }
-  // }
-  
-  export default MyApp
+
+  return app.currentUser.accessToken;
+}
+
+// Configure the ApolloClient to connect to your app's GraphQL endpoint
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: `https://realm.mongodb.com/api/client/v2.0/app/${process.env.REALM_APP_ID}/graphql`,
+    // We define a custom fetch handler for the Apollo client that lets us authenticate GraphQL requests.
+    // The function intercepts every Apollo HTTP request and adds an Authorization header with a valid
+    // access token before sending the request.
+    fetch: async (uri, options) => {
+      const accessToken = await getValidAccessToken();
+      options.headers.Authorization = `Bearer ${accessToken}`;
+      return fetch(uri, options);
+    },
+  }),
+  cache: new InMemoryCache(),
+});
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} client={client} />;
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
+
+export default MyApp;
